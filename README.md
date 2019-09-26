14b42c96584d72b4a8c78f963ebd9ab38e8d2265
surenderthakran <surender.thakran89@gmail.com>
2019-09-26 19:27:02 +0530

    Finished adding problem to compare printable string outputs of keypresses

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 ##### 1. (Difficulty: 2) [Find intersection of two sorted array of integers.](sortedArrayIntersection/main.java) Time Complexity: `O(n)`
 ##### 2. (Difficulty: 3) [Alphabetize (sort alphabetically) lowercase characters in a given string.](alphabetizeLowercaseChars/main.java) Time Complexity: `O(n log(n))`
-##### 3. (Difficulty: 2) [Given two arrays of keypresses, determine if they output same printable string.](compareKeypressArrays/main.java) Time Complexity:
+##### 3. (Difficulty: 2) [Given two arrays of keypresses, determine if they output same printable string.](compareKeypressArrays/main.java) Time Complexity: `O(n)`
--- compareKeypressArrays/main.java
+++ compareKeypressArrays/main.java
@@ -1,3 +1,43 @@
+/**
+ * Given two arrays of keypresses, determine if they would print the same string.
+ *
+ * The keypress arrays can only contain lowercase characters and backspace character represented by
+ * '#';
+ */
+
 class Main {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    boolean assertEnabled = false;
+    assert assertEnabled = true;
+
+    if (assertEnabled) {
+      assert
+          isKeypressArraysEqual(
+              new char[]{'a', 'b', 'c', '#', 'd'},
+              new char[]{'a', 'c', '#', 'b', 'd'}) == true;
+
+      System.out.println("All Assertions Succeeded!");
+    } else {
+      System.out.println("Asserions not enabled! Results not verified!");
+    }
+  }
+
+  private static boolean isKeypressArraysEqual(char[] one, char[] two) {
+    return getPrintableString(one).equals(getPrintableString(two));
+  }
+
+  private static String getPrintableString(char[] keypresses) {
+    StringBuilder stringBuilder = new StringBuilder();
+
+    // Appending and deleting characters from StringBuilder takes constant time `O(n)`.
+    for (char key : keypresses) {
+      if (key == '#' && stringBuilder.length() > 0) {
+        stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+      } else {
+        stringBuilder.append(key);
+      }
+    }
+
+    return stringBuilder.toString();
+  }
 }
