31963c939734b6ec480d4a45fc1860c95d494b22
surenderthakran <surender.thakran89@gmail.com>
2019-09-27 08:53:51 +0530

    Updated hexadecimal addition problem.

--- hexadecimalAddition/main.java
+++ hexadecimalAddition/main.java
@@ -2,6 +2,9 @@
  * Given two hexadecimal strings, return their sum in hexadecimal format.
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 class Main {
   public static void main(String[] args) {
     boolean assertEnabled = false;
@@ -18,6 +21,50 @@ class Main {
   }
 
   private static String hexadecimalAddition(String one, String two) {
-    return "abc";
+    StringBuilder oneBuilder = new StringBuilder(one.toLowerCase());
+    StringBuilder twoBuilder = new StringBuilder(two.toLowerCase());
+    StringBuilder result = new StringBuilder();
+
+    char carry = '0';
+    while (oneBuilder.length() > 0 || twoBuilder.length() > 0) {
+      char first = '0';
+      if (oneBuilder.length() > 0) {
+        first = oneBuilder.charAt(oneBuilder.length() - 1);
+        oneBuilder.deleteCharAt(oneBuilder.length() - 1);
+      }
+
+      char second = '0';
+      if (twoBuilder.length() > 0) {
+        second = twoBuilder.charAt(twoBuilder.length() - 1);
+        twoBuilder.deleteCharAt(twoBuilder.length() - 1);
+      }
+
+      String sum = addHexaNumbers(carry, first, second);
+      carry = sum.charAt(0);
+      result.append(sum.charAt(1));
+    }
+
+    return result.toString();
+  }
+
+  private static String addHexaNumbers(char carry, char first, char second) {
+    List<Character> hexadecimals = new ArrayList<>();
+    hexadecimals.add('0');
+    hexadecimals.add('1');
+    hexadecimals.add('2');
+    hexadecimals.add('3');
+    hexadecimals.add('4');
+    hexadecimals.add('5');
+    hexadecimals.add('6');
+    hexadecimals.add('7');
+    hexadecimals.add('8');
+    hexadecimals.add('9');
+    hexadecimals.add('a');
+    hexadecimals.add('b');
+    hexadecimals.add('c');
+    hexadecimals.add('d');
+    hexadecimals.add('e');
+    hexadecimals.add('f');
+    return "00";
   }
 }
