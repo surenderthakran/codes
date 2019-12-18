355b78fd296d4e92ad1e0cf6c9e52132b7aa03f2
surenderthakran <surender.thakran89@gmail.com>
2019-12-18 09:27:00 +0530

    Updated new class design which supports inserts, random peek and random delete operations.

--- classWithInsertRandomPeekAndDeleteCapabilities/main.java
+++ classWithInsertRandomPeekAndDeleteCapabilities/main.java
@@ -7,13 +7,12 @@
  */
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 
 enum Colour {
-  RED, BLUE, GREEN, YELLOW, ORANGE, BLACK, BROWN
+  RED, BLUE, GREEN
 }
 
 class Marble {
@@ -30,34 +29,107 @@ class Marble {
 
 class Jar {
   List<Marble> marbleList;
-  Map<Marble, Integer> marbleMap;
 
   Jar() {
     this.marbleList = new ArrayList<Marble>();
-    this.marbleMap = new HashMap<Marble, Integer>();
   }
 
   void insertMarbles(Marble[] marbles) {
     for (Marble marble : marbles) {
       marbleList.add(marble);
-      marbleMap.put(marble, marbleList.size() - 1);
     }
   }
 
-  Marble peekRandomMarble() {}
+  Marble peekRandomMarble() {
+    return this.marbleList.get(this.getRandomListIndex());
+  }
 
-  Marble removeRandomMarble() {}
+  Marble removeRandomMarble() {
+    return this.marbleList.remove(this.getRandomListIndex());
+  }
 
-  private Marble getRandomFromList() {
+  int getSize() {
+    return this.marbleList.size();
+  }
 
+  Colour[] getMarbleColours() {
+    return this.marbleList.stream()
+      .map(marble -> marble.getColour())
+      .toArray(Colour[]::new);
+  }
+
+  private int getRandomListIndex() {
+    Random random = new Random();
+    return random.ints(0, this.marbleList.size()).findFirst().getAsInt();
   }
 }
 
 class Main {
   public static void main (String[] args) {
-    Marble red = new Marble(Colour.RED);
+    Marble red1 = new Marble(Colour.RED);
+    Marble red2 = new Marble(Colour.RED);
+    Marble red3 = new Marble(Colour.RED);
+
+    Marble blue1 = new Marble(Colour.BLUE);
+    Marble blue2 = new Marble(Colour.BLUE);
+    Marble blue3 = new Marble(Colour.BLUE);
+    Marble blue4 = new Marble(Colour.BLUE);
+
+    Marble green1 = new Marble(Colour.GREEN);
+    Marble green2 = new Marble(Colour.GREEN);
 
     Jar jar = new Jar();
-    jar.insertMarbles(new Marble[]{red});
+    jar.insertMarbles(new Marble[]{red1, blue1, green1, red2, blue2, green2, red3, blue3, blue4});
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
+
+    System.out.println(jar.peekRandomMarble().getColour());
+    System.out.println(jar.removeRandomMarble().getColour());
+
+    System.out.println("Jar size: " + jar.getSize());
+    System.out.println(Arrays.toString(jar.getMarbleColours()));
   }
 }
