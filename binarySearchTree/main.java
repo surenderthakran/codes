8adbd54590522a27d29cf6a16f10aaa70d3a973b
surenderthakran <surender.thakran89@gmail.com>
2019-12-25 22:23:23 +0530

    Updated binarySearchTree.

--- binarySearchTree/main.java
+++ binarySearchTree/main.java
@@ -1,8 +1,25 @@
 /**
  * Implementing a Binary Search Tree.
+ *
+ * For a tree to be a binary search tree, it must be:
+ *  1. A binary tree
+ *  2. For each node, the left child should be smaller than the current node.
+ *  3. For each node, the right child should be greater than the current node.
+ *  4. For each node, every node in the left subtree should be smaller than the current node.
+ *  5. For each node, every node in the right subtree should be smaller than the current node.
+ *
+ *  Simply making sure that each node's left child is smaller and right child is larger is not
+ *  enough, since a left child can be smaller than the current node but the left child's right
+ *  child could be greater than its grand parent node.
+ *
+ *  There are two methods for validating a Binary Search Tree:
+ *  1. In-Order Traversal Method
+ *  2. Range Limit Method
  */
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 class Node {
   private int value;
@@ -45,6 +62,12 @@ class BinarySearchTree {
     return this.root;
   }
 
+  /**
+   * Checks if the binary search tree contains the given integer.
+   *
+   * @param num Integer to check for existence.
+   * @return True if the integer exists in the tree, else false;
+   */
   boolean contains(int num) {
     Node current = this.root;
     while (true) {
@@ -61,6 +84,64 @@ class BinarySearchTree {
       }
     }
   }
+
+  /**
+   * Determines validity of the Binary Search Tree using the in-order traversal method.
+   *
+   * For a valid Binary Search Tree, in-order traversal should mean a sorted traversal. In this
+   * method, we will traverse the tree in-order and check if each encountered node is larger than
+   * or equal to the previous node.
+   *
+   * @return True if the tree is a valid Binary Search Tree, else false.
+   */
+  boolean isValidBSTInOrderTraversalMethod() {
+    List<Node> inOrderNodes = getNodesInOrder();
+    Node lastNode = null;
+
+    // Check if in-order list of nodes is sorted.
+    for (Node node : inOrderNodes) {
+      if (lastNode != null && lastNode.getValue() > node.getValue()) {
+        return false;
+      }
+      lastNode = node;
+    }
+
+    return true;
+  }
+
+  /**
+   * Returns an in-order  List of all nodes of the tree.
+   */
+  List<Node> getNodesInOrder() {
+    return getNodesInOrder(this.root);
+  }
+
+  /**
+   * Returns an in-order List of all nodes of the tree which has the given node as root.
+   *
+   * @param node Root node of the current (sub-)tree.
+   * @return In-order list of nodes.
+   */
+  List<Node> getNodesInOrder(Node node) {
+    List<Node> inOrderNodes = new ArrayList<>();
+
+    // Add nodes from left sub-tree.
+    if (node.getLeft() != null) {
+      inOrderNodes.addAll(getNodesInOrder(node.getLeft()));
+    }
+
+    // Add current node.
+    inOrderNodes.add(node);
+
+    // Add nodes from right sub-tree.
+    if (node.getRight() != null) {
+      inOrderNodes.addAll(getNodesInOrder(node.getRight()));
+    }
+
+    return inOrderNodes;
+  }
+
+  boolean isValidBSTRangeLimitMethod() {}
 }
 
 class Main {
@@ -108,9 +189,7 @@ class Main {
     assert tree.contains(1) == true;
     assert tree.contains(10) == false;
 
-    // assert tree.isValidBinarySearchTree() == true;
-
-    // This is sorted since the tree is a binary search tree.
-    // assert Arrays.equals(tree.getDataInorder(), new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9});
+    assert tree.isValidBSTInOrderTraversalMethod() == true;
+    assert tree.isValidBSTRangeLimitMethod() == true;
   }
 }
