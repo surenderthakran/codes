09f6aeac7a4c45dec6a8cb59adaf8ee58dc0d767
surenderthakran <surender.thakran89@gmail.com>
2020-01-26 16:17:38 +0530

    Updated binarySearchTree

--- binarySearchTree/main.java
+++ binarySearchTree/main.java
@@ -19,225 +19,53 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 class Node {
   private int value;
-  private Node leftChild;
-  private Node rightChild;
+  private Optional<Node> leftChild;
+  private Optional<Node> rightChild;
 
   Node(int value) {
     this.value = value;
+    this.leftChild = Optional.empty();
+    this.rightChild = Optional.empty();
   }
 
   int getValue() {
     return this.value;
   }
 
-  Node getLeft() {
+  Optional<Node> getLeft() {
     return this.leftChild;
   }
 
   void setLeft(Node node) {
-    this.leftChild = node;
+    this.leftChild = Optional.of(node);
   }
 
-  Node getRight() {
+  Optional<Node> getRight() {
     return this.rightChild;
   }
 
   void setRight(Node node) {
-    this.rightChild = node;
+    this.rightChild = Optional.of(node);
   }
 }
 
 class BinarySearchTree {
-  private Node root;
+  private Optional<Node> root;
 
   BinarySearchTree() {
-    this.root = null;
+    this.root = Optional.empty();
   }
 
   BinarySearchTree(Node root) {
-    this.root = root;
+    this.root = Optional.of(root);
   }
 
-  Node getRoot() {
+  Optional<Node> getRoot() {
     return this.root;
-  }
-
-  /**
-   * Checks if the binary search tree contains the given integer.
-   *
-   * @param num Integer to check for existence.
-   * @return True if the integer exists in the tree, else false;
-   */
-  boolean contains(int num) {
-    Node current = this.root;
-    while (true) {
-      if (current.getValue() == num) {
-        return true;
-      } else if (current.getValue() > num && current.getLeft() != null) {
-        current = current.getLeft();
-        continue;
-      } else if (current.getValue() < num && current.getRight() != null) {
-        current = current.getRight();
-        continue;
-      } else {
-        return false;
-      }
-    }
-  }
-
-  void insert(int num) {
-    Node newNode = new Node(num);
-    if (this.root == null) {
-      this.root = newNode;
-      return;
-    }
-
-    Node current = this.root;
-    while (true) {
-      if (num == current.getValue()) {
-        if (current.getLeft() != null) {
-          newNode.setLeft(current.getLeft());
-        }
-        current.setLeft(newNode);
-        return;
-      } else if (num < current.getValue()) {
-        if (current.getLeft() == null) {
-          current.setLeft(newNode);
-          return;
-        }
-        current = current.getLeft();
-      } else if (num > current.getValue()) {
-        if (current.getRight() == null) {
-          current.setRight(newNode);
-          return;
-        }
-        current = current.getRight();
-      }
-    }
-  }
-
-  /**
-   * Determines validity of the Binary Search Tree using the in-order traversal method.
-   *
-   * For a valid Binary Search Tree, in-order traversal should mean a sorted traversal. In this
-   * method, we will traverse the tree in-order and check if each encountered node is larger than
-   * or equal to the previous node.
-   *
-   * @return True if the tree is a valid Binary Search Tree, else false.
-   */
-  boolean isValidBSTInOrderTraversalMethod() {
-    List<Node> inOrderNodes = getNodesInOrder();
-    Node lastNode = null;
-
-    // Check if in-order list of nodes is sorted.
-    for (Node node : inOrderNodes) {
-      if (lastNode != null && lastNode.getValue() > node.getValue()) {
-        return false;
-      }
-      lastNode = node;
-    }
-
-    return true;
-  }
-
-  /**
-   * Returns an in-order  List of all nodes of the tree.
-   */
-  List<Node> getNodesInOrder() {
-    return getNodesInOrder(this.root);
-  }
-
-  /**
-   * Returns an in-order List of all nodes of the tree which has the given node as root.
-   *
-   * @param node Root node of the current (sub-)tree.
-   * @return In-order list of nodes.
-   */
-  List<Node> getNodesInOrder(Node node) {
-    List<Node> inOrderNodes = new ArrayList<>();
-
-    // Add nodes from left sub-tree.
-    if (node.getLeft() != null) {
-      inOrderNodes.addAll(getNodesInOrder(node.getLeft()));
-    }
-
-    // Add current node.
-    inOrderNodes.add(node);
-
-    // Add nodes from right sub-tree.
-    if (node.getRight() != null) {
-      inOrderNodes.addAll(getNodesInOrder(node.getRight()));
-    }
-
-    return inOrderNodes;
-  }
-
-  /**
-   * Determines validity of the whole Binary Search Tree using the range limiting method.
-   *
-   * For a valid Binary Search Tree, each node should fall within the limits defined by its parents.
-   * And all nodes in it's left sub-tree should fall between current nodes minimum range value and
-   * current nodes value. Similarly for right sub-tree, all nodes should fall between current nodes
-   * value and current nodes maximum range value.
-   *
-   * @return True if the tree is a valid Binary Search Tree, else false.
-   */
-  boolean isValidBSTRangeLimitMethod() {
-    return isValidBSTRangeLimitMethod(this.root, Integer.MIN_VALUE, Integer.MAX_VALUE);
-  }
-
-  /**
-   * Determines validity of the Binary Search Tree with the given node as root and given min & max
-   * values using the range limiting method.
-   *
-   * @return True if the tree is a valid Binary Search Tree, else false.
-   */
-  boolean isValidBSTRangeLimitMethod(Node node, int min, int max) {
-    // Check if current node is in the range.
-    if (node.getValue() < min || node.getValue() > max) {
-      return false;
-    }
-
-    // Check if the left sub-tree is in range.
-    if (node.getLeft() != null
-        && !isValidBSTRangeLimitMethod(node.getLeft(), min, node.getValue())) {
-      return false;
-    }
-
-    // Check if the right sub-tree is in range.
-    if (node.getRight() != null
-        && !isValidBSTRangeLimitMethod(node.getRight(), node.getValue(), max)) {
-      return false;
-    }
-
-    return true;
-  }
-}
-
-class Main {
-  public static void main(String[] args) {
-    boolean assertEnabled = false;
-    assert assertEnabled = true;
-
-    if (assertEnabled) {
-      testValidBST();
-      testInValidBST();
-
-      testValidBSTCreatedUsingInsertions();
-
-      // Create BST from a pre-order list of node values.
-      BinarySearchTree tree = createBSTFromPreOrderArray(new int[]{10, 5, 1, 7, 40, 50});
-      // Assert is tree is a valid Binary Search Tree.
-      assert tree.isValidBSTInOrderTraversalMethod() == true;
-      assert tree.isValidBSTRangeLimitMethod() == true;
-
-      System.out.println("All Assertions Succeeded!");
-    } else {
-      System.out.println("Asserions not enabled! Results not verified!");
-    }
   }
 
   /**
@@ -286,14 +114,207 @@ class Main {
     // If the sub-array for the left sub-tree values has adequate number of values, create a BST
     // from them and set it's root as current root's left child.
     if (first + 1 <= rightChildIndex - 1) {
-      root.setLeft(createBSTFromPreOrderArray(preorder, first + 1, rightChildIndex - 1).getRoot());
+      root.setLeft(
+          createBSTFromPreOrderArray(preorder, first + 1, rightChildIndex - 1).getRoot().get());
     }
 
     // Create a BST from the right sub-tree elements in the pre-order array and set it's root as
     // current root's right child.
-    root.setRight(createBSTFromPreOrderArray(preorder, rightChildIndex, last).getRoot());
+    root.setRight(
+        createBSTFromPreOrderArray(preorder, rightChildIndex, last).getRoot().get());
 
     return tree;
+  }
+
+  /**
+   * Checks if the binary search tree contains the given integer.
+   *
+   * @param num Integer to check for existence.
+   * @return True if the integer exists in the tree, else false;
+   */
+  boolean contains(int num) {
+    if(!this.root.isPresent()) {
+      return false;
+    }
+
+    Node current = this.root.get();
+    while (true) {
+      if (current.getValue() == num) {
+        return true;
+      } else if (current.getValue() > num && current.getLeft().isPresent()) {
+        current = current.getLeft().get();
+        continue;
+      } else if (current.getValue() < num && current.getRight().isPresent()) {
+        current = current.getRight().get();
+        continue;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  void insert(int num) {
+    Node newNode = new Node(num);
+    if (!this.root.isPresent()) {
+      this.root = Optional.of(newNode);
+      return;
+    }
+
+    Node current = this.root.get();
+    while (true) {
+      if (num == current.getValue()) {
+        if (current.getLeft().isPresent()) {
+          newNode.setLeft(current.getLeft().get());
+        }
+        current.setLeft(newNode);
+        return;
+      } else if (num < current.getValue()) {
+        if (!current.getLeft().isPresent()) {
+          current.setLeft(newNode);
+          return;
+        }
+        current = current.getLeft().get();
+      } else if (num > current.getValue()) {
+        if (!current.getRight().isPresent()) {
+          current.setRight(newNode);
+          return;
+        }
+        current = current.getRight().get();
+      }
+    }
+  }
+
+  /**
+   * Determines validity of the Binary Search Tree using the in-order traversal method.
+   *
+   * For a valid Binary Search Tree, in-order traversal should mean a sorted traversal. In this
+   * method, we will traverse the tree in-order and check if each encountered node is larger than
+   * or equal to the previous node.
+   *
+   * @return True if the tree is a valid Binary Search Tree, else false.
+   */
+  boolean isValidBSTInOrderTraversalMethod() {
+    List<Node> inOrderNodes = getNodesInOrder();
+    if (inOrderNodes.size() == 0) {
+      return false;
+    }
+
+    Node lastNode = null;
+
+    // Check if in-order list of nodes is sorted.
+    for (Node node : inOrderNodes) {
+      if (lastNode != null && lastNode.getValue() > node.getValue()) {
+        return false;
+      }
+      lastNode = node;
+    }
+
+    return true;
+  }
+
+  /**
+   * Returns an in-order  List of all nodes of the tree.
+   */
+  List<Node> getNodesInOrder() {
+    if (!this.root.isPresent()) {
+      return new ArrayList<>();
+    }
+    return getNodesInOrder(this.root.get());
+  }
+
+  /**
+   * Returns an in-order List of all nodes of the tree which has the given node as root.
+   *
+   * @param node Root node of the current (sub-)tree.
+   * @return In-order list of nodes.
+   */
+  List<Node> getNodesInOrder(Node node) {
+    List<Node> inOrderNodes = new ArrayList<>();
+
+    // Add nodes from left sub-tree.
+    if (node.getLeft().isPresent()) {
+      inOrderNodes.addAll(getNodesInOrder(node.getLeft().get()));
+    }
+
+    // Add current node.
+    inOrderNodes.add(node);
+
+    // Add nodes from right sub-tree.
+    if (node.getRight().isPresent()) {
+      inOrderNodes.addAll(getNodesInOrder(node.getRight().get()));
+    }
+
+    return inOrderNodes;
+  }
+
+  /**
+   * Determines validity of the whole Binary Search Tree using the range limiting method.
+   *
+   * For a valid Binary Search Tree, each node should fall within the limits defined by its parents.
+   * And all nodes in it's left sub-tree should fall between current nodes minimum range value and
+   * current nodes value. Similarly for right sub-tree, all nodes should fall between current nodes
+   * value and current nodes maximum range value.
+   *
+   * @return True if the tree is a valid Binary Search Tree, else false.
+   */
+  boolean isValidBSTRangeLimitMethod() {
+    if (!this.root.isPresent()) {
+      return false;
+    }
+    return isValidBSTRangeLimitMethod(this.root.get(), Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  /**
+   * Determines validity of the Binary Search Tree with the given node as root and given min & max
+   * values using the range limiting method.
+   *
+   * @return True if the tree is a valid Binary Search Tree, else false.
+   */
+  boolean isValidBSTRangeLimitMethod(Node node, int min, int max) {
+    // Check if current node is in the range.
+    if (node.getValue() < min || node.getValue() > max) {
+      return false;
+    }
+
+    // Check if the left sub-tree is in range.
+    if (node.getLeft().isPresent()
+        && !isValidBSTRangeLimitMethod(node.getLeft().get(), min, node.getValue())) {
+      return false;
+    }
+
+    // Check if the right sub-tree is in range.
+    if (node.getRight().isPresent()
+        && !isValidBSTRangeLimitMethod(node.getRight().get(), node.getValue(), max)) {
+      return false;
+    }
+
+    return true;
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    boolean assertEnabled = false;
+    assert assertEnabled = true;
+
+    if (assertEnabled) {
+      testEmptyBST();
+      testValidBST();
+      testInValidBST();
+      testValidBSTCreatedUsingInsertions();
+      testValidBSTCreatedFromPreOrderArray();
+
+      System.out.println("All Assertions Succeeded!");
+    } else {
+      System.out.println("Asserions not enabled! Results not verified!");
+    }
+  }
+
+  static void testEmptyBST() {
+    BinarySearchTree tree = new BinarySearchTree();
+
+    assert tree.isValidBSTInOrderTraversalMethod() == false;
+    assert tree.isValidBSTRangeLimitMethod() == false;
   }
 
   static void testValidBST() {
@@ -356,7 +377,16 @@ class Main {
     tree.insert(10);
     tree.insert(5);
     tree.insert(15);
+    tree.insert(5);
 
+    assert tree.isValidBSTInOrderTraversalMethod() == true;
+    assert tree.isValidBSTRangeLimitMethod() == true;
+  }
+
+  static void testValidBSTCreatedFromPreOrderArray() {
+    // Create BST from a pre-order array of node values.
+    BinarySearchTree tree =
+        BinarySearchTree.createBSTFromPreOrderArray(new int[]{10, 5, 1, 7, 40, 50});
 
     assert tree.isValidBSTInOrderTraversalMethod() == true;
     assert tree.isValidBSTRangeLimitMethod() == true;
