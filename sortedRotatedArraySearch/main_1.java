d94f1ae7b1f5c7defa5e6d3985a4bd9f369e62c3
surenderthakran <surender.thakran89@gmail.com>
2020-02-14 09:18:46 +0530

    Updated sortedRotatedArraySearch

--- sortedRotatedArraySearch/main_1.java
+++ sortedRotatedArraySearch/main_1.java
@@ -3,7 +3,8 @@
  *
  * The method used in this solution is:
  * 1. Determine if the array is actually rotated. If not, move to step 5. (O (1))
- * 2. If the array is rotated, find the pivot point (largest number) in the array. (O (log n))
+ * 2. If the array is rotated, find the pivot index (index with a smaller number on it's right) in
+ *    the array. (O (log n))
  * 2. Determine which side of the pivot would the required number fall. (O (1))
  * 3. Perform a binary search for the number on that sorted portion of the array. (O (log n))
  *
@@ -31,50 +32,77 @@ class Main {
   }
 
   static int indexOf(int num, int[] arr) {
+    if (arr.length == 1) {
+      if (arr[0] == num) {
+        return 0;
+      } else {
+        return -1;
+      }
+    }
+
     // If the array is not rotated.
     if (arr[0] < arr[arr.length - 1]) {
       return binarySearch(num, arr);
     }
 
-    // Find the pivot point in the array using binary search.
-    int pivot = -1;
-    int left = 0;
-    int right = arr.length - 1;
-    while (left <= right) {
-      int mid = (left + right)/2;
+    // Note: Since the array is rotated, pivot can never be the last index of the array.
+    int pivot = getPivotIndex(arr);
 
-      // If mid itself has the required number, we return mid as result from here itself.
-      if (arr[mid] == num) {
-        return mid;
-      }
-
-      // If mid itself is the pivot.
-      if (arr[mid] > arr[mid + 1]) {
-        pivot = mid;
-        break;
-      }
-
-      // Reset left or right index to continue searching for pivot.
-      if (left == mid - 1 && arr[left] > arr[mid]) {  // If left side has only one element. Check.
-        pivot = left;
-        break;
-      } else if (arr[left] > arr[mid - 1]) {          // Pivot lies on the left of the mid.
-        right = mid - 1;
-      } else if (arr[mid + 1] > arr[right]) {         // Pivot lies on the right of the mid.
-        left = mid + 1;
-      }
+    if (pivot == num) {
+      return pivot;
     }
 
     // Determine which side of the pivot would the required number fall and perform a binary search
     // for the number on that side.
     if (pivot > 0 && arr[0] <= num && num <= arr[pivot - 1]) {
       return binarySearch(num, arr, 0, pivot - 1);
-    } else if (
-        pivot < arr.length - 1 && arr[pivot + 1] <= num && num <= arr[arr.length - 1]) {
+    } else if (arr[pivot + 1] <= num && num <= arr[arr.length - 1]) {
       return binarySearch(num, arr, pivot + 1, arr.length - 1);
     }
 
     return -1;
+  }
+
+  static int getPivotIndex(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+    return getPivotIndex(arr, left, right);
+  }
+
+  static int getPivotIndex(int[] arr, int left, int right) {
+    int mid = (left + right) / 2;
+
+    // If mid itself is the pivot.
+    if ( arr[mid] > arr [mid + 1]) {
+      return mid;
+    }
+
+    int pivotOnLeft = -1;
+    int pivotOnRight = -1;
+
+    if (left != mid) {
+      if (left == mid - 1) {
+        if(arr[left] > arr [mid]) {
+          return left;
+        }
+      } else if (arr[left] >= arr[mid - 1]) {           // Search for pivot in the left half only if
+                                                        // it is not irrefutably sorted.
+        pivotOnLeft = getPivotIndex(arr, left, mid - 1);
+      }
+    }
+
+    if (right == mid + 1) {
+      // Since we had earlier established that the array is rotated, hence the pivot cannot be the
+      // last index of the array.
+      if (right != arr.length - 1 && arr[right] > arr[right + 1]) {
+        return right;
+      }
+    } else if (arr[mid + 1] >= arr[right]) {          // Search for pivot in the right half only if
+                                                      // it is not irrefutably sorted.
+      pivotOnRight = getPivotIndex(arr, mid + 1, right);
+    }
+
+    return pivotOnLeft > pivotOnRight ? pivotOnLeft : pivotOnRight;
   }
 
   static int binarySearch(int num, int[] arr) {
