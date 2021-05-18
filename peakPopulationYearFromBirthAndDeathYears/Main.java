3c05df98a041a0755b02492ced2babf5a23fb3d2
Surender Thakran <surender.thakran89@gmail.com>
2021-05-18 09:42:48 +0530

    Added brute force solution for peakPopulationYearFromBirthAndDeathYears problem.

---
+++ peakPopulationYearFromBirthAndDeathYears/Main.java
@@ -0,0 +1,51 @@
+package com.surenderthakran.coding.peakpopulationyearfrombirthanddeathyears;
+
+import java.util.Arrays;
+
+/**
+ * Given birth and death years of a group of people, find the year(s) when most people were alive
+ * i.e. the year[s) with the highest population.
+ *
+ * <p>Assumptions:
+ *
+ * <ul>
+ *   <li>A person is assumed to be alive on their birth year and their death year.
+ * </ul>
+ */
+class Main {
+
+  public static void main(String[] args) {
+    boolean assertsEnabled = false;
+    assert assertsEnabled = true;
+
+    if (assertsEnabled) {
+      assert areEqualIgnoringOrder(
+          SolutionBruteForce.getPeakPopulationYears(
+              new int[][] {
+                {1905, 1947},
+                {1857, 1910},
+                {1863, 1921},
+                {1814, 1899},
+                {1914, 1945},
+                {1918, 1961},
+                {1801, 1813},
+                {2000, 2021},
+                {1857, 1911},
+                {1905, 1921}
+              }),
+          new int[] {
+            1905, 1906, 1907, 1908, 1909, 1910, 1918, 1919, 1920, 1921
+          }); // peak population is 5.
+
+      System.out.println("All Assertions Succeeded!");
+    } else {
+      System.out.println("Asserions not enabled! Results not verified!");
+    }
+  }
+
+  private static boolean areEqualIgnoringOrder(int[] first, int[] second) {
+    Arrays.sort(first);
+    Arrays.sort(second);
+    return Arrays.equals(first, second);
+  }
+}
---
+++ peakPopulationYearFromBirthAndDeathYears/SolutionBruteForce.java
@@ -0,0 +1,41 @@
+package com.surenderthakran.coding.peakpopulationyearfrombirthanddeathyears;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class SolutionBruteForce {
+
+  static int[] getPeakPopulationYears(int[][] persons) {
+    HashMap<Integer, Integer> yearToPopulationMap = new HashMap<>();
+    HashSet<Integer> yearsWithPeakPopulations = new HashSet<>();
+    int peakPopulation = 0;
+
+    for (int[] person : persons) {
+      for (int year = person[0]; year <= person[1]; year++) {
+        if (yearToPopulationMap.containsKey(year)) {
+          int population = yearToPopulationMap.get(year) + 1;
+          yearToPopulationMap.put(year, population);
+
+          if (population > peakPopulation) {
+            peakPopulation = population;
+            yearsWithPeakPopulations.clear();
+            yearsWithPeakPopulations.add(year);
+          } else if (population == peakPopulation) {
+            yearsWithPeakPopulations.add(year);
+          }
+        } else {
+          yearToPopulationMap.put(year, 1);
+        }
+      }
+    }
+
+    System.out.printf("peakPopulation: %d \n", peakPopulation);
+    int[] years = new int[yearsWithPeakPopulations.size()];
+    int i = 0;
+    for (int year : yearsWithPeakPopulations) {
+      years[i] = year;
+      i++;
+    }
+    return years;
+  }
+}
